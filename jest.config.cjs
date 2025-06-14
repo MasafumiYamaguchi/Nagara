bad95d9c2ecd6a2f77f3ed3ffb414c292b1eb2a7
Masafumi Yamaguchi <al24031@shibaura-it.ac.jp>
2025-06-14 21:55:25 +0900

    add: babelとjestの設定ファイルを追加

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,6 +9,9 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
+  modulePathIgnorePatterns: [
+    '<rootDir>/frontend/',
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@testing-library/react-native|react-native-reanimated|react-native-gesture-handler)/).*'
   ],
