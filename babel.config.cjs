99af9d23a9103579e0ce7f850032f839465c10b1
Masafumi Yamaguchi <al24031@shibaura-it.ac.jp>
2025-06-26 00:50:24 +0900

    fix: jestが通らない問題を解決

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,12 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'nativewind/babel',
+module.exports = function (api) {
+  api.cache(true);
+  const presets = ['module:metro-react-native-babel-preset', 'nativewind/babel'];
+  const plugins = [
     'react-native-reanimated/plugin',
-  ],
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,3 @@
-// 必要なモックだけを残して、問題のあるインポートをコメントアウト
-// import 'react-native-gesture-handler/jestSetup';
-// import '@testing-library/jest-native/extend-expect';
-
 // Reanimated のモック設定
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
@@ -9,6 +5,5 @@ jest.mock('react-native-reanimated', () => {
   return Reanimated;
 });
 
-// 問題のあるモックをコメントアウト
-// jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper', () => {});
-
+// @testing-library/jest
+require('@testing-library/jest-native/extend-expect');
